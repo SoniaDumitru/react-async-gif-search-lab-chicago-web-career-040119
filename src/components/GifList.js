4419aade85fc516f8836069ab0849fe9e09d8e7a
Sonia Dumitru <soni.dumitru@gmail.com>
2019-06-09 21:58:15 -0500

    Done.

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react'
 
+//GifList component child of GifListContainer component
+
+//is a presentational component, it receives data from props and renders each image in a HTML
 export default class GifList extends Component {
+
   render() {
     return(
       <ul>
         {/*map over data and render each image in a li*/}
         { this.props.gifs.map(gif =>
-          <li>
-            <img src={gif.url} width="100%" key={gif.url}/>
-          </li>) }
+          <li><img src={gif.url} width="50%" key={gif.url}/></li>) }
       </ul>
     )
   }
